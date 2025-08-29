c488b42f64251c1fcf8c046b08ca8564b64e04d0
isa-id <juanse.giraldo07@gmail.com>
2025-08-28 21:38:44 -0500

    3U7

--- frontend/src/components/layouts/Header.jsx
+++ frontend/src/components/layouts/Header.jsx
@@ -5,7 +5,7 @@ import "./Header.css";
 export default function Header() {
   return (
     <header className="game-header">
-      <h1 className="game-title"> 3UT</h1>
+      <h1 className="game-title"> 3U7</h1>
       <PointsDisplay />
     </header>
   );

0c20cfb49c9950caeb66fd9f754a0b3bc84e3a7e
JUAN SEBASTIAN GIRALDO CARDONA <juanse.giraldo07@gmail.com>
2026-03-24 15:40:07 -0500

    Configure base path for GitHub Pages deployment
    Set base path for asset routes in GitHub Pages.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,14 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/EUT/", // Asegura que las rutas de assets funcionen en isa-id.github.io/EUT/
   resolve: {
     alias: {
       "@context": path.resolve(__dirname, "src/context"),
       "@components": path.resolve(__dirname, "src/components"),
       "@game": path.resolve(__dirname, "src/game"),
       "@data": path.resolve(__dirname, "src/data"),
-      '@': path.resolve(__dirname, './src'),  
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
